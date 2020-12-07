82222ceb71ced97c1ca4b098e3b967ce44c2280a
Roman Nikitin <nikrom2012@me.com>
2020-12-07 19:10:58 +0300

    More consistent rule creation in tcp

--- internal/proxy/tcp/filters/default.go
+++ internal/proxy/tcp/filters/default.go
@@ -5,10 +5,10 @@ var DefaultRules = map[string]Rule{
 	"egress":  &CompositeNotRule{new(IngressRule)},
 }
 
-var DefaultRuleFactories = map[string]RuleFactory{
-	"ingress": IngressRuleFactory,
-	"egress":  EgressRuleFactory,
-	"not":     NotRuleFactory,
+var DefaultRuleWrappers = map[string]RuleWrapperCreator{
+	"ingress": NewIngressWrapper,
+	"egress":  NewEgressWrapper,
+	"not":     NewNotWrapper,
 }
 
 var DefaultRuleCreators = map[string]RuleCreator{
--- internal/proxy/tcp/filters/interface.go
+++ internal/proxy/tcp/filters/interface.go
@@ -12,7 +12,7 @@ type Rule interface {
 
 type RuleCreator func(cfg *common.RuleConfig) (Rule, error)
 type CompositeRuleCreator func(rs *RuleSet, cfg *common.RuleConfig) (Rule, error)
-type RuleFactory func(creator RuleCreator) RuleCreator
+type RuleWrapperCreator func(rule Rule, cfg *common.RuleConfig) Rule
 
 type RuleSet struct {
 	Rules map[string]Rule
@@ -37,33 +37,39 @@ func NewRuleSet(cfg []*common.RuleConfig) (*RuleSet, error) {
 			if len(tokens) < 2 {
 				return nil, fmt.Errorf("invalid rule: %s", rc.Type)
 			}
-			// Create base rule creator first
-			baseName := tokens[len(tokens)-1]
 
-			creator, ok := DefaultRuleCreators[baseName]
-			if !ok {
-				var compositeCreator CompositeRuleCreator
-				if compositeCreator, ok = DefaultCompositeRuleCreators[baseName]; ok {
-					creator = compositeCreator.PartialConvert(rs)
+			var (
+				ok               bool
+				err              error
+				rule             Rule
+				compositeCreator CompositeRuleCreator
+				creator          RuleCreator
+			)
+
+			// the last rule in chain must be either the composite rule or some rule creator
+			lastToken := tokens[len(tokens)-1]
+			if compositeCreator, ok = DefaultCompositeRuleCreators[lastToken]; ok {
+				// rule is composite
+				if rule, err = compositeCreator(rs, rc); err != nil {
+					return nil, fmt.Errorf("creating rule %s: %w", lastToken, err)
 				}
-			}
-			if !ok {
-				return nil, fmt.Errorf("unknown rule type: %s", rc.Type)
+			} else if creator, ok = DefaultRuleCreators[lastToken]; ok {
+				if rule, err = creator(rc); err != nil {
+					return nil, fmt.Errorf("creating raw rule %s: %w", lastToken, err)
+				}
+			} else {
+				return nil, fmt.Errorf("invalid rule %s: last token invalid", rc.Type)
 			}
 
 			for i := len(tokens) - 2; i > 0; i -= 1 {
-				factoryName := tokens[i]
-				factory, ok := DefaultRuleFactories[factoryName]
+				wrapperName := tokens[i]
+				wrapper, ok := DefaultRuleWrappers[wrapperName]
 				if !ok {
-					return nil, fmt.Errorf("invalid factory name: %s", factoryName)
+					return nil, fmt.Errorf("invalid wrapper name: %s", wrapperName)
 				}
-				creator = factory(creator)
+				rule = wrapper(rule, rc)
 			}
 
-			rule, err := creator(rc)
-			if err != nil {
-				return nil, fmt.Errorf("creating rule %s: %w", rc.Type, err)
-			}
 			rs.Rules[rc.Name] = rule
 		}
 	}
