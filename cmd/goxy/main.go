5eb4034df54bd9099d63cda1e6059b2b676b9335
Roman Nikitin <nikrom2012@me.com>
2020-12-07 20:15:17 +0300

    Add api server base

--- cmd/goxy/main.go
+++ cmd/goxy/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"context"
 	"flag"
+	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"goxy/internal/common"
 	"goxy/internal/proxy"
+	"goxy/internal/web"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
@@ -18,6 +21,33 @@ var (
 	configFile = flag.String("config", "config.yml", "Path to the config file in YAML format")
 	logLevel   = flag.String("log_level", "DEBUG", "Log level {INFO|DEBUG|WARNING|ERROR}")
 )
+
+func main() {
+	flag.Parse()
+
+	setLogLevel()
+	setWebServerMode()
+	setConfigDefaults()
+	parseConfig()
+
+	cfg := parseProxyConfig()
+	m := runProxyManager(cfg)
+
+	s := web.NewServer(m)
+	httpServer := startHttpServer(s)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+	<-c
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	shutdownHttpServer(httpServer, ctx)
+	shutdownProxyManager(m, ctx)
+
+	logrus.Info("Shutdown successful")
+}
 
 func setLogLevel() {
 	switch strings.ToUpper(*logLevel) {
@@ -36,6 +66,21 @@ func setLogLevel() {
 	}
 }
 
+func setWebServerMode() {
+	level := logrus.StandardLogger().GetLevel()
+	if level == logrus.DebugLevel {
+		gin.SetMode(gin.DebugMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+}
+
+func setConfigDefaults() {
+	viper.SetDefault("web.static_dir", "front/dist")
+	viper.SetDefault("web.username", "admin")
+	viper.SetDefault("web.listen", "0.0.0.0:8000")
+}
+
 func parseConfig() {
 	viper.SetConfigFile(*configFile)
 	viper.SetConfigType("yaml")
@@ -44,17 +89,15 @@ func parseConfig() {
 	}
 }
 
-func main() {
-	flag.Parse()
-
-	setLogLevel()
-	parseConfig()
-
+func parseProxyConfig() *common.ProxyConfig {
 	cfg := new(common.ProxyConfig)
 	if err := viper.Unmarshal(&cfg); err != nil {
 		logrus.Fatal("Error parsing proxy config from file: ", err)
 	}
+	return cfg
+}
 
+func runProxyManager(cfg *common.ProxyConfig) *proxy.Manager {
 	m, err := proxy.NewManager(cfg)
 	if err != nil {
 		logrus.Fatalf("Error creating proxy manager: %v", err)
@@ -62,16 +105,38 @@ func main() {
 	if err := m.StartAll(); err != nil {
 		logrus.Fatalf("Error starting proxy manager: %v", err)
 	}
+	return m
+}
 
-	c := make(chan os.Signal, 1)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
-	<-c
+func startHttpServer(s *web.Server) *http.Server {
+	srv := &http.Server{
+		Addr:         viper.GetString("web.listen"),
+		ReadTimeout:  time.Second * 15,
+		WriteTimeout: time.Second * 15,
+		IdleTimeout:  time.Second * 30,
+		Handler:      s,
+	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
-	defer cancel()
+	go func() {
+		logrus.Infof("Serving on http://%s", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logrus.Fatal("Error serving api server: ", err)
+		}
+	}()
+
+	return srv
+}
+
+func shutdownHttpServer(srv *http.Server, ctx context.Context) {
+	logrus.Info("Shutting down http server")
+	if err := srv.Shutdown(ctx); err != nil {
+		logrus.Fatalf("Error shutting down http server: %v", err)
+	}
+}
+
+func shutdownProxyManager(m *proxy.Manager, ctx context.Context) {
 	logrus.Info("Shutting down proxies")
 	if err := m.Shutdown(ctx); err != nil {
 		logrus.Fatalf("Error shutting down proxies: %v", err)
 	}
-	logrus.Info("Shutdown successful")
 }
---
+++ internal/web/server.go
@@ -0,0 +1,74 @@
+package web
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"goxy/internal/proxy"
+	"net/http"
+	"os"
+	"path"
+)
+
+func NewServer(pm *proxy.Manager) *Server {
+	ms := &Server{
+		ProxyManager: pm,
+		Router:       gin.New(),
+		StaticDir:    viper.GetString("web.static_dir"),
+		AuthData: &BasicAuthData{
+			Username: viper.GetString("web.username"),
+			Password: viper.GetString("web.password"),
+		},
+	}
+	ms.registerMiddleware()
+	ms.registerRoutes()
+	return ms
+}
+
+type BasicAuthData struct {
+	Username string
+	Password string
+}
+
+type Server struct {
+	ProxyManager *proxy.Manager
+	Router       *gin.Engine
+	StaticDir    string
+	AuthData     *BasicAuthData
+}
+
+func (s *Server) registerRoutes() {
+	logrus.Infof("Serving static dir: %s", s.StaticDir)
+
+	s.Router.NoRoute(func(c *gin.Context) {
+		realPath := path.Join(s.StaticDir, c.Request.URL.Path)
+		if _, err := os.Stat(realPath); os.IsNotExist(err) {
+			realPath = path.Join(s.StaticDir, "index.html")
+		}
+		c.File(realPath)
+	})
+
+	logrus.Info("Routes registered successfully")
+}
+
+func (s *Server) registerMiddleware() {
+	s.Router.Use(gin.Recovery())
+	s.Router.Use(loggerMiddleware())
+
+	s.Router.Use(gzip.Gzip(gzip.DefaultCompression))
+	s.Router.Use(cors.Default())
+
+	if !gin.IsDebugging() {
+		s.Router.Use(gin.BasicAuth(gin.Accounts{
+			s.AuthData.Username: s.AuthData.Password,
+		}))
+	}
+
+	logrus.Info("Middleware registered successfully")
+}
+
+func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.Router.ServeHTTP(w, r)
+}
